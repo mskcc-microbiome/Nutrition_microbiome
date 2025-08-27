a9b55d843c19661c7f23213e368fa7c8624bc506
baichom1 <baichom1@mskcc.org>
2025-08-27 15:28:45 -0400

    adding in clinical cov and adjusting appearance

--- scripts/R37_covariates_effect_beta_div.R
+++ scripts/R37_covariates_effect_beta_div.R
@@ -29,10 +29,14 @@ otu_table <- read.csv(paste0(proj_home, "/data/R25_asv_counts.csv")) %>%
 otu_table[is.na(otu_table)] <- 0
 dist_mat <- as.matrix(vegdist(t(otu_table), method = "bray"))
 
+clinical_cov <- readRDS(paste0(proj_home, "/data/R02_cleaned_clinical_outcome.rds")) %>%
+  select(c(source,intensity, age, sex, disease.simple, modal_diet, pid))
 
-metadata <- read.csv(paste0(proj_home, "/data/153_combined_META.csv")) %>%
-  mutate(empirical = factor(empirical, levels = c(F, T))) %>%
-  mutate(intensity = factor(intensity, levels = c("nonablative", "reduced", "ablative")))
+metadata <- read.csv(paste0(proj_home, "/data/153_combined_META.csv")) %>% 
+  select(c(sampleid, empirical, timebin, fg_egg, fg_fruit, fg_grain, fg_legume, fg_meat, fg_milk, fg_oils, fg_sweets, fg_veggie, TPN, EN, pid)) %>%
+  left_join(clinical_cov, by = "pid") %>%
+  select(-pid)
+    
 
 # make sure sample order is the same:
 reorder_indices <- match(rownames(dist_mat), metadata$sampleid)
@@ -45,11 +49,27 @@ factor_fit <- vegan::envfit(as.data.frame(dist_mat), metadata, perms=1000)
 plotting_data_frame <- find_covariates_using_covariates_matrix_and_dist_mat(dist_mat, metadata, threshold = threshold, perms = 100)
 
 plotting_data_frame %>%
-   filter(significant) %>%
+   filter(significant) %>% 
+  mutate(variable_name = case_when(
+    variable_name == "timebin" ~ "Week Relative to Transplant",
+    variable_name == "disease.simple" ~ "Disease (Broad Category)",
+    variable_name == "empirical" ~ "ABX exposure (last 2 days)",
+    variable_name == "modal diet" ~ "Dietary Pattern",
+    variable_name == "source" ~ "Graft Source",
+    variable_name == "intensity" ~ "Conditioning Intensity",
+    variable_name == "EN" ~ "Enteral Nutrition",
+    variable_name == "TPN" ~ "Total Parenteral Nutrition",
+    variable_name == "sex" ~ "Sex",
+    variable_name == "age" ~ "Age",
+    .default = variable_name
+  )
+  ) %>%
    mutate(  # Modify me to define your categories of interest! :) 
      variable_category = case_when(
        grepl("fg", variable_name) ~ "Dietary Data",
-       variable_name %in% c("simpson reciprocal") ~ "Stool Diversity\n(lol should\nremove this,\nleaving for\ndemo purposes)",
+       variable_name %in% c("Dietary Pattern", "Enteral Nutrition", "Total Parenteral Nutrition") ~ "Dietary Data",
+       variable_name %in% c("ABX exposure (last 2 days)", "Week Relative to Transplant") ~ "Sample Level Data",
+       variable_name %in% c("Graft Source", "Conditioning Intensity", "Disease (Broad Category)", "Sex", "Age") ~ "Patient Clinical Cov",
        .default = "Other"
      )
    ) %>%
@@ -61,13 +81,13 @@ plotting_data_frame %>%
         y = paste0("FDR Signficant Covariate, p < ", threshold),
         fill = "Covariate\nCategory")+
    theme(
-     axis.text = element_text(size = 24), # Adjust axis labels
+     axis.text = element_text(size = 18), # Adjust axis labels
      axis.title = element_text(size = 24), # Adjust axis titles
      plot.title = element_text(size = 24),  # Adjust plot title
      strip.text.x = element_text(size = 20),
      legend.text = element_text(size = 15),
      legend.title  = element_text(size = 20),
-     strip.text.y = element_text(size = 20)
+     strip.text.y = element_text(size = 17)
    ) 
 
                   
